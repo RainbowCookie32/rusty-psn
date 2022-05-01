c833563037e61cb9e064de8ad619816c9db42a66
Matias Moran <matimoran03@gmail.com>
2022-05-01 14:58:54 -0300

    utils: add some comments so i don't forget why code is the way it is

--- src/utils.rs
+++ src/utils.rs
@@ -23,26 +23,30 @@ pub async fn create_pkg_file(path: PathBuf) -> Result<File, DownloadError> {
         }
     }
 
-    let mut options = OpenOptions::default();
-        
-    options.create(true);
-    options.read(true);
-    options.write(true);
-    options.open(path).await.map_err(DownloadError::Tokio)
+    // Using OpenOptions to avoid the file getting truncated if it already exists
+    // .create(true) preserves an existing file's contents.
+    OpenOptions::default()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open(path).await.map_err(DownloadError::Tokio)
 }
 
 pub async fn hash_file(file: &mut File, hash: &str) -> Result<bool, DownloadError> {
     let mut buf = Vec::new();
     let mut hasher = Sha1::new();
 
+    // Write operations during the download move the internal seek pointer.
+    // Resetting it to 0 makes .read_to_end actually read the whole thing.
     file.seek(SeekFrom::Start(0)).await.map_err(DownloadError::Tokio)?;
 
+    // If the amount of data read is below the length of the embedded sha1-hash,
+    // don't bother hashing the contents. Download's borked.
     if file.read_to_end(&mut buf).await.map_err(DownloadError::Tokio)? < 0x20 {
         return Ok(false);
     }
     
     // Last 0x20 bytes are the SHA1 hash.
     hasher.update(&buf[0..buf.len() - 0x20]);
-
     Ok(hasher.digest().to_string() == hash)
 }
